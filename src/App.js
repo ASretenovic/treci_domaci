a0a6611e286120aae09d15ea61f5237124fd644c
asretenovic000@gmail.com <asretenovic000@gmail.com>
2023-01-01 21:27:54 +0100

    Header. Responzivnost.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact element={<Pocetna />} />
+          <Route path='/' exact element />
         </Routes>
       </Router>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from '../images/logo.png';
 import { Link } from 'react-router-dom';
+import ReorderIcon from '@mui/icons-material/Reorder';
 import '../styles/Navbar.css';
 
 function Navbar() {
+
+    const [activeLinks, setLinksActive] = useState(false);
+
+    // funkcija menja status promenljive activeLinks svaki put kada se klikne na dugme
+    const manageNavbar = () => {
+        setLinksActive(!activeLinks);
+    };
+
   return (
     <div className='navbar'>
-            <div className='left-column'>
-              <img src={logo}/>
+          <div className='left-column' id={activeLinks ? 'open' : 'close'}>
+              <img src={logo} />
+              <div className='hiddenLinks'>
+                {/* linkovi koji ce se prikazivati samo kad se smanji rezolucija */}
+                <Link to='/'>Početna</Link>
+                <Link to='/proizvodi'>Proizvodi</Link>
+                <Link to='/kontakt'>Kontakt</Link>
+                <Link to='/oNama'>O nama</Link>
+              </div>
             </div>
 
             <div className='right-column'>
@@ -15,6 +31,9 @@ function Navbar() {
               <Link to='/proizvodi'>Proizvodi</Link>
               <Link to='/kontakt'>Kontakt</Link>
               <Link to='/oNama'>O nama</Link>
+              <button onClick={manageNavbar}>
+                  <ReorderIcon />
+              </button>
             </div>
     </div>
   )
